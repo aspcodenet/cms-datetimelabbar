29562ee14fe270b494033a494a69009af8166b36
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-07 09:23:12 +0200

    Add project files.

---
+++ DateTimeLabbar/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeLabbar
+{
+    class Program
+    {
+        static void Lab1()
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-mm-dd HH:mm:SS"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy-mm-dd"));
+        }
+        static void Lab2()
+        {
+            var dt = DateTime.Now;
+            Console.WriteLine( $@"year = {dt.Year}                                                                      
+month = {dt.Month}                                                                        
+day = {dt.Day}                                                                         
+hour = {dt.Hour}                                                                         
+minute = {dt.Minute}                                                                      
+second = {dt.Second}                                                                      
+millisecond = {dt.Millisecond}   ");
+            
+        }
+
+        static void Lab3()
+        {
+            DateTime dt = DateTime.Now;
+            string veckoDagsString = dt.DayOfWeek.ToString();
+
+            Console.WriteLine($"Dagens veckodag är {veckoDagsString}");
+        }
+
+        static void Lab4()
+        {
+            Console.WriteLine($"Today is {DateTime.Now.ToString("yyyy-mm-dd HH:mm:SS")}");
+
+            DateTime dt = DateTime.Now.AddDays(40);
+            Console.WriteLine($"40 days from now is {dt.DayOfWeek.ToString()}");
+            Console.WriteLine(dt.ToString("yyyy-mm-dd HH:mm:SS"));
+        }
+
+        static void Lab5()
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt.ToString("yyyy-mm-dd"));
+            for (var i = 0; i < 10; i++)
+            {
+                dt = dt.AddDays(40);
+                Console.WriteLine(dt.ToString("yyyy-mm-dd"));
+            }
+        }
+
+        static Random rnd = new Random();
+        static void Lab7()
+        {
+            DateTime d = new DateTime(rnd.Next(1970,2016),rnd.Next(1,12), rnd.Next(1,28));
+
+            var oneMonthForward = new DateTime(d.Year, d.Month, d.Day).AddMonths(1);
+            var firstOfThat = new DateTime(oneMonthForward.Year, oneMonthForward.Month, 1);
+            var lastOfBefore = firstOfThat.AddDays(-1);
+
+            Console.WriteLine($"Year is {lastOfBefore.Year}, month is {lastOfBefore.Month} and it has {lastOfBefore.Day} days");
+
+        }
+
+        static void Lab8()
+        {
+            DateTime dq = new DateTime(rnd.Next(1970, 2016), rnd.Next(1, 12), rnd.Next(1, 28));
+            DateTime dw = new DateTime(rnd.Next(1970, 2016), rnd.Next(1, 12), rnd.Next(1, 28));
+
+            TimeSpan sp = dq - dw;
+            Console.WriteLine(dq.ToString("yyyy-mm-dd"));
+            Console.WriteLine(dw.ToString("yyyy-mm-dd"));
+            Console.WriteLine($"difference is {sp.TotalDays} days");
+
+        }
+
+        static void Lab6()
+        {
+            Console.Write("Ange datum:");
+            string txt = Console.ReadLine();
+            DateTime dt = DateTime.ParseExact(txt, "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture);
+            Console.WriteLine(dt.ToString("yyyy-mm-dd"));
+        }
+
+        static void Lab9()
+        {
+            Console.Write("Ange år:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ange månad(1-12):");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ange day(1-31):");
+            int day = Convert.ToInt32(Console.ReadLine());
+            if(DateTime.Now.Year == year && DateTime.Now.Month == month && DateTime.Now.Day == day)
+            {
+                Console.WriteLine("Detta är idag");
+            }
+            else
+                Console.WriteLine("Detta är INTE idag");
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Lab1();
+            //Lab2();
+            //Lab3();
+            //Lab4();
+            Lab5();
+            Lab6();
+            //for(int i =0; i < 100; i++)
+            //    Lab7();
+
+            //Lab8();
+            Lab9();
+            Console.ReadLine();
+        }
+    }
+}
