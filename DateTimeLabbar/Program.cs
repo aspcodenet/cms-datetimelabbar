accb8d9a3b7c28fc4e4f080432b0450c122ffe08
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-16 12:50:22 +0200

    Buggfixar mm -> MM

--- DateTimeLabbar/Program.cs
+++ DateTimeLabbar/Program.cs
@@ -10,13 +10,14 @@ namespace DateTimeLabbar
     {
         static void Lab1()
         {
-            Console.WriteLine(DateTime.Now.ToString("yyyy-mm-dd HH:mm:SS"));
-            Console.WriteLine(DateTime.Now.ToString("yyyy-mm-dd"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:SS"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd"));
         }
         static void Lab2()
         {
             var dt = DateTime.Now;
-            Console.WriteLine( $@"year = {dt.Year}                                                                      
+
+            Console.WriteLine($@"year = {dt.Year}                                                                      
 month = {dt.Month}                                                                        
 day = {dt.Day}                                                                         
 hour = {dt.Hour}                                                                         
@@ -32,6 +33,34 @@ millisecond = {dt.Millisecond}   ");
             string veckoDagsString = dt.DayOfWeek.ToString();
 
             Console.WriteLine($"Dagens veckodag är {veckoDagsString}");
+
+            string inSwedish = "";
+            switch(dt.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    inSwedish = "Måndag";
+                    break;
+                case DayOfWeek.Tuesday:
+                    inSwedish = "Tisdag";
+                    break;
+                case DayOfWeek.Wednesday:
+                    inSwedish = "Onsdag";
+                    break;
+                case DayOfWeek.Thursday:
+                    inSwedish = "Torsdag";
+                    break;
+                case DayOfWeek.Friday:
+                    inSwedish = "Fredag";
+                    break;
+                case DayOfWeek.Saturday:
+                    inSwedish = "Lördag";
+                    break;
+                case DayOfWeek.Sunday:
+                    inSwedish = "Söndag";
+                    break;
+
+            }
+
         }
 
         static void Lab4()
@@ -46,18 +75,19 @@ millisecond = {dt.Millisecond}   ");
         static void Lab5()
         {
             DateTime dt = DateTime.Now;
-            Console.WriteLine(dt.ToString("yyyy-mm-dd"));
+            Console.WriteLine(dt.ToString("yyyy-MM-dd"));
             for (var i = 0; i < 10; i++)
             {
                 dt = dt.AddDays(40);
-                Console.WriteLine(dt.ToString("yyyy-mm-dd"));
+                Console.WriteLine(dt.ToString("yyyy-MM-dd"));
             }
         }
 
         static Random rnd = new Random();
         static void Lab7()
         {
-            DateTime d = new DateTime(rnd.Next(1970,2016),rnd.Next(1,12), rnd.Next(1,28));
+            //DateTime d = new DateTime(rnd.Next(1970,2016),rnd.Next(1,12), rnd.Next(1,28));
+            DateTime d = DateTime.Now;
 
             var oneMonthForward = new DateTime(d.Year, d.Month, d.Day).AddMonths(1);
             var firstOfThat = new DateTime(oneMonthForward.Year, oneMonthForward.Month, 1);
@@ -73,6 +103,7 @@ millisecond = {dt.Millisecond}   ");
             DateTime dw = new DateTime(rnd.Next(1970, 2016), rnd.Next(1, 12), rnd.Next(1, 28));
 
             TimeSpan sp = dq - dw;
+
             Console.WriteLine(dq.ToString("yyyy-mm-dd"));
             Console.WriteLine(dw.ToString("yyyy-mm-dd"));
             Console.WriteLine($"difference is {sp.TotalDays} days");
@@ -83,8 +114,9 @@ millisecond = {dt.Millisecond}   ");
         {
             Console.Write("Ange datum:");
             string txt = Console.ReadLine();
-            DateTime dt = DateTime.ParseExact(txt, "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture);
-            Console.WriteLine(dt.ToString("yyyy-mm-dd"));
+            DateTime dt = DateTime.ParseExact(txt, "yyyy-MM-dd", 
+                System.Globalization.CultureInfo.InvariantCulture);
+            Console.WriteLine(dt.ToString("yyyy-MM-dd"));
         }
 
         static void Lab9()
@@ -95,7 +127,10 @@ millisecond = {dt.Millisecond}   ");
             int month = Convert.ToInt32(Console.ReadLine());
             Console.Write("Ange day(1-31):");
             int day = Convert.ToInt32(Console.ReadLine());
-            if(DateTime.Now.Year == year && DateTime.Now.Month == month && DateTime.Now.Day == day)
+
+            var inputDate = new DateTime(year, month, day);
+
+            if(DateTime.Now.Date == inputDate)
             {
                 Console.WriteLine("Detta är idag");
             }
@@ -106,9 +141,10 @@ millisecond = {dt.Millisecond}   ");
 
         static void Main(string[] args)
         {
+            Lab5();
             //Lab1();
             //Lab2();
-            //Lab3();
+            Lab3();
             //Lab4();
             Lab5();
             Lab6();
